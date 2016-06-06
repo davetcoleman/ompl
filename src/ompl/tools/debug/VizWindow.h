18a63cb5f08df6c27309629265450b40e0283c0b
Dave Coleman <davetcoleman@gmail.com>
2016-06-06 16:56:04 -0600

    Made functions pure virtual

--- src/ompl/tools/debug/VizWindow.h
+++ src/ompl/tools/debug/VizWindow.h
@@ -114,30 +114,28 @@ namespace ompl
                 OMPL_INFORM("Loading VizWindow()");
             }
 
-            /** \brief Visualize planner's data during runtime, externally */
-            void state(const ompl::base::State *state, VizSizes type, VizColors color, double extraData)
-            {
-            }
+            /** \brief Visualize a state during runtime, externally */
+            virtual void state(const ompl::base::State *state, VizSizes type, VizColors color, double extraData) = 0;
 
-            /** \brief Visualize planner's data during runtime, externally */
-            void edge(const ompl::base::State *stateA, const ompl::base::State *stateB, double cost)
-            {
-            }
+            /** \brief Visualize a state with a level during runtime, externally */
+            virtual void state(const ompl::base::State *state, std::size_t level, VizSizes type, VizColors color,
+                               double extraData) = 0;
 
-            /** \brief Visualize planner's data during runtime, externally */
-            void path(ompl::geometric::PathGeometric *path, std::size_t type, VizColors color)
-            {
-            }
+            /** \brief Visualize edge during runtime, externally */
+            virtual void edge(const ompl::base::State *stateA, const ompl::base::State *stateB, double cost) = 0;
 
-            /** \brief Trigger visualizer to publish graphics */
-            void trigger()
-            {
-            }
+            /** \brief Visualize edge with a level during runtime, externally */
+            virtual void edge(const ompl::base::State* stateA, std::size_t levelA, const ompl::base::State* stateB,
+                std::size_t levelB, ompl::tools::VizSizes size, ompl::tools::VizColors color) = 0;
 
-            /** \brief Trigger visualizer to publish graphics */
-            void deleteAllMarkers()
-            {
-            }
+            /** \brief Visualize path during runtime, externally */
+            virtual void path(ompl::geometric::PathGeometric *path, std::size_t type, VizColors color) = 0;
+
+            /** \brief Trigger visualizer to refresh/repaint/display all graphics */
+            virtual void trigger() = 0;
+
+            /** \brief Trigger visualizer to clear all graphics */
+            virtual void deleteAllMarkers() = 0;
 
         };  // end of class VizWindow
     }       // namespace tools
